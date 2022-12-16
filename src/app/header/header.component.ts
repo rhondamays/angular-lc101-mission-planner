bdc43353c481962bb4de4f6aa84462c5d84d8422
Rhonda Mays <maysrhonda.7@gmail.com>
2022-12-15 20:09:03 -0600

    updated mission planner for part 3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
-  editingMission: boolean = false;
+  missionName: string = "LC Angular 3";
+  rocketName: string = "ICAN";
   editingRocket: boolean = false;
-
+  editingMission: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
@@ -26,5 +26,4 @@ export class HeaderComponent implements OnInit {
     this.rocketName = updatedName;
     this.editingRocket = false;
   }
-
 }
